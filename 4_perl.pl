62fdc5a0f00a4d84973f49d1f0d433c64e633cc1
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-19 20:49:00 +0000

    Also benchmark misses

--- 4_perl.pl
+++ 4_perl.pl
@@ -7,8 +7,10 @@ use Time::HiRes qw/ gettimeofday tv_interval /;
 
 my $ordalisti = 'ordalisti.txt';
 my $lines     = count_lines($ordalisti);
+my $matcher;
 
 # Progress bar
+{
 my $progress = Term::Sk->new('%d Elapsed: %8t %21b %4p %2d (%8c of %11m)', {
     # Start at line 1, not 0
     base => 1,
@@ -30,27 +32,45 @@ while (my $ord = <$listi>) {
 }
 $progress->close;
 my $giant_regex = $ra->re;
-my $matcher = qr/^(?:$giant_regex)$/;
-
-# Make the user ask us stuff
-say "Ask if your word matches:";
-while (my $query = <STDIN>) {
-    $query->chomp;
-    my $sorted = $query->split(qr//)->sort->join("");
-    my $start_time = [gettimeofday()];
-
-    # Check if we got a match
-    my $matched = $sorted ~~ $matcher;
-    given ($matched) {
-        when (1) {
-            say "<$query> (<$sorted>) matched a word in our db";
-        }
-        default {
-            say "No match for <$query> (<$sorted>)";
-        }
-    }
-
-    my $elapsed = tv_interval($start_time);
-    say sprintf "Replied %.10f seconds", $elapsed;
+$matcher = qr/^(?:$giant_regex)$/;
 }
 
+# Benchmark questions
+open my $listi, "cat $ordalisti | iconv -f iso-8859-1 -t utf-8 |";
+
+my $time_to_match = 0;
+
+# Make the user ask us stuff
+#say "Ask if your word matches:";
+#while (my $query = <STDIN>) {
+my %match;
+while (my $query = <$listi>) {
+    $query->chomp;
+    my $sorted = $query->split(qr//)->sort->join("");
+
+    # Fuzz some words so not all will match, benchmarks misses
+    $sorted =~ s/a/b/g;
+
+    # Check if we got a match
+    my $start_time = [gettimeofday()];
+    my $matched = $sorted ~~ $matcher;
+    my $elapsed = tv_interval($start_time);
+    $time_to_match += $elapsed;
+
+    $match{$matched}++;
+
+    # given ($matched) {
+    #     when (1) {
+    #         my 
+    #         say "<$query> (<$sorted>) matched a word in our db";
+    #     }
+    #     default {
+    #         say "No match for <$query> (<$sorted>)";
+    #     }
+    # }
+
+    # say sprintf "Replied %.10f seconds", $elapsed;
+}
+
+say sprintf "Matched $lines words took %.4f seconds, or %.8f seconds per word", $time_to_match, $time_to_match / $lines;
+say sprintf "Out of %d queries %d matched a word in our db, %d did not", $lines, $match{'1'}, $match{''};
