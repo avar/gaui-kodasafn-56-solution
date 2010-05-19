a67efdf3f08a1de940f547edafbb1adcccc4eebc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-19 21:22:11 +0000

    Made 5.8 compatible, and print regex debug output if uncommented

--- 4_perl.pl
+++ 4_perl.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/env perl
-use perl5i::latest;
+use open qw/ :encoding(utf8) :std /;
+use strict;
+use warnings;
 use File::CountLines qw/ count_lines /;
 use Term::Sk;
 use Regexp::Assemble;
 use Time::HiRes qw/ gettimeofday tv_interval /;
+
+sub say { print shift, "\n" }
 
 my $ordalisti = 'ordalisti.txt';
 my $interactive = $ARGV[0];
@@ -18,25 +22,29 @@ my $progress = Term::Sk->new('%d Elapsed: %8t %21b %4p %2d (%8c of %11m)', {
     target => $lines,
     # Every 0.1 seconds for long files
     freq => ($lines < 10_000 ? 10 : 'd'),
-}) or die "Error in Term::Sk->new: (code $Term::Sk::errcode) $Term::Sk::errmsg";
+});
+
 
 # Convert to UTF-8
 open my $listi, "cat $ordalisti | iconv -f iso-8859-1 -t utf-8 |";
 
 # Make giant optimized regex
+
 my $ra = Regexp::Assemble->new;
 while (my $ord = <$listi>) {
-    $ord->chomp;
-    my $sorted = $ord->split(qr//)->sort->join("");
+    chomp $ord;
+    my $sorted = join('', sort split //, $ord);
     $progress->up;
-    $ra->add( $sorted->quotemeta );
+    $ra->add( quotemeta $sorted );
 }
 $progress->close;
 
 say "Making giant regex...";
 my $start_time = [gettimeofday()];
 my $giant_regex = $ra->re;
+#use re 'debug';
 $matcher = qr/^(?:$giant_regex)$/;
+#no re 'debug';
 
 my $elapsed = tv_interval($start_time);
 say sprintf "Made regex in %.4f seconds", $elapsed;
@@ -54,8 +62,8 @@ say "Ask if your word matches:";
 $listi = *STDIN;
 }
 while (my $query = <$listi>) {
-    $query->chomp;
-    my $sorted = $query->split(qr//)->sort->join("");
+    chomp $query;
+    my $sorted = join '', sort split //, $query;
 
     say "  Matching <$query> as <$sorted>..";
 
@@ -64,7 +72,7 @@ while (my $query = <$listi>) {
 
     # Check if we got a match
     my $start_time = [gettimeofday()];
-    my $matched = $sorted ~~ $matcher;
+    my $matched = $sorted =~ $matcher;
     my $elapsed = tv_interval($start_time);
     $time_to_match += $elapsed;
 
